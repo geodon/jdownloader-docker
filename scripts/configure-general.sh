9c9f375af09a8d75c977185b9b7ecaeaa985adf3
Geodon <1690053+geodon@users.noreply.github.com>
2025-11-17 21:46:55 +0100

    feat: add default general settings for better user experience
    - Enable automatic download start (ALWAYS)
    - Enable hash checking for integrity verification
    - Skip files if they already exist (SKIP_FILE)
    - Clean up after package finishes
    - Create folders on download start
    - Set skipped files as successful

--- scripts/configure-general.sh
+++ scripts/configure-general.sh
@@ -11,6 +11,12 @@ RECONNECT_CONFIG="jd.controlling.reconnect.ReconnectConfig.json"
 
 # Set download folder to the Downloads volume with package name subfolder
 json_set_property "$GENERAL_CONFIG" "defaultdownloadfolder" "/Downloads/<jd:packagename>"
+json_set_property "$GENERAL_CONFIG" "cleanupafterdownloadaction" "CLEANUP_AFTER_PACKAGE_HAS_FINISHED"
+json_set_property "$GENERAL_CONFIG" "autostartdownloadoption" "ALWAYS"
+json_set_property "$GENERAL_CONFIG" "hashcheckenabled" "true"
+json_set_property "$GENERAL_CONFIG" "iffileexistsaction" "SKIP_FILE"
+json_set_property "$GENERAL_CONFIG" "createfoldertrigger" "ON_DOWNLOAD_START"
+json_set_property "$GENERAL_CONFIG" "onskipduetoalreadyexistsaction" "SET_FILE_TO_SUCCESSFUL"
 
 # Disable auto-reconnect (not useful in Docker environment)
 json_set_property "$RECONNECT_CONFIG" "autoreconnectenabled" "false"
